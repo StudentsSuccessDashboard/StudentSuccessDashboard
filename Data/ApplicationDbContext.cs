6e484020d664abefcdac7221d9098b3c0ba7d659
SammyYim <SAMMYCYIM@outlook.com>
2026-07-17 17:02:41 -0700

    Initial Supabase setup

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StudentSuccessDashboard.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
